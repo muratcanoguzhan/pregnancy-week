21e0182fa870d7e2bf756bfcff957265bc1c4dd4
Murat Can OĞUZHAN <m.c.ogzhan@gmail.com>
2023-11-01 00:44:31 +0100

    Highlight.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { DatePipe, NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy, signal } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
@@ -18,9 +18,10 @@ import { RouterOutlet } from '@angular/router';
     MatDividerModule,
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'pregnancy-week';
 
+  color = signal('primary');
   pregnancyWeeks = new Array<PregnancyWeek>();
   icons: string[] = [
     '&#x1F476;',
@@ -30,28 +31,68 @@ export class AppComponent {
     '&#x1F383;',
     '&#x1F468;&#x200D;&#x1F469;&#x200D;&#x1F467;',
   ];
-  constructor() {
-    let startOffset = 8;
-    let startDate = new Date(2023, 9, 19);
-    const today = this.addDate(new Date(), 4 * 7);
+  colors = [
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+    'light',
+    'dark',
+  ];
 
-    while (startDate < today) {
+  interval: number;
+  constructor() {
+    //let startDate = new Date(2023, 7, 23);//actual date
+    let startDate = new Date(2023, 7, 24, 0, 0, 0);
+    let endDate = this.addDate(startDate, 6, true);
+    const today = new Date();
+
+    for (let i = 0; i < 42; i++) {
       const pregnancyWeek = new PregnancyWeek();
-      pregnancyWeek.date = startDate;
-      pregnancyWeek.weekNo = startOffset;
+      pregnancyWeek.startDate = startDate;
+      pregnancyWeek.endDate = endDate;
+      pregnancyWeek.weekNo = i + 1;
+
+      if (
+        startDate.getTime() <= today.getTime() &&
+        today.getTime() <= endDate.getTime()
+      ) {
+        debugger;
+        pregnancyWeek.isCurrentWeek = true;
+      }
       this.pregnancyWeeks.push(pregnancyWeek);
 
-      startDate = this.addDate(startDate, 7);
-      startOffset++;
+      startDate = this.addDate(endDate, 1);
+      endDate = this.addDate(startDate, 6);
     }
+
+    this.interval = window.setInterval(() => {
+      const index = this.colors.findIndex((c) => c == this.color());
+      this.color.set(this.colors[(index + 1) % this.colors.length]);
+    }, 850);
+  }
+  ngOnDestroy(): void {
+    window.clearInterval(this.interval);
   }
 
-  addDate(date: Date, days: number): Date {
-    return new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
+  addDate(date: Date, days: number, endOfTheDateTime: boolean = false): Date {
+    if (endOfTheDateTime) {
+      return new Date(
+        date.getTime() +
+          days * 24 * 60 * 60 * 1000 +
+          (23 * 60 * 60 * 1000 + 9999)
+      );
+    } else {
+      return new Date(date.getTime() + days * 24 * 60 * 60 * 1000);
+    }
   }
 }
 
 export class PregnancyWeek {
-  date: Date | undefined;
+  startDate: Date | undefined;
+  endDate: Date | undefined;
   weekNo: number | undefined;
+  isCurrentWeek: boolean = false;
 }
